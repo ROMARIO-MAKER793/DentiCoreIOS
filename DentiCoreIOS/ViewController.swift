b1bcad77e0ebc597cd6d22565ac9cff344f53d71
Robert Contreras Mamani <rcontrerasmamani@gmail.com>
2026-08-18 19:56:23 -0500

    Initial Commit

---
+++ DentiCoreIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DentiCoreIOS
+//
+//  Created by XCODE on 18/08/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
