d35ae93940f278c70c3db3f00bf2284bc484c7dc
Robert Contreras Mamani <rcontrerasmamani@gmail.com>
2026-08-19 19:57:58 -0500

    DCIOS-11 Integrar autenticacion JWT con SpringBoot

--- DentiCoreIOS/LoginViewController.swift
+++ DentiCoreIOS/LoginViewController.swift
@@ -35,12 +35,72 @@ class LoginViewController: UIViewController {
     
     
     @IBAction func btnIniciarSesion(_ sender: Any) {
+
         guard let dni = textFieldDNI.text,
-                  let password = textFieldPassword.text,
-                  !dni.isEmpty,
-                  !password.isEmpty else {
+              let password = textFieldPassword.text,
+              !dni.isEmpty,
+              !password.isEmpty else {
+            return
+        }
+
+        let loginRequest = LoginRequest(
+            dni: dni,
+            password: password
+        )
+
+        guard let url = URL(
+            string: "http://localhost:8080/api/v1/auth/login"
+        ) else {
+            return
+        }
+
+        var request = URLRequest(url: url)
+
+        request.httpMethod = "POST"
+
+        request.setValue(
+            "application/json",
+            forHTTPHeaderField: "Content-Type"
+        )
+
+        do {
+            request.httpBody = try JSONEncoder().encode(loginRequest)
+        } catch {
+            print("Error al convertir JSON: \(error)")
+            return
+        }
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+
+            if let error = error {
+                print("Error de conexión: \(error)")
                 return
             }
+
+            guard let data = data else {
+                return
+            }
+
+            do {
+
+                let loginResponse = try JSONDecoder().decode(
+                    LoginResponse.self,
+                    from: data
+                )
+
+                print("LOGIN CORRECTO")
+                print("ROL: \(loginResponse.rol)")
+                print("TOKEN: \(loginResponse.token)")
+
+            } catch {
+                print("Error al leer respuesta: \(error)")
+
+                if let respuesta = String(data: data, encoding: .utf8) {
+                    print("Respuesta backend: \(respuesta)")
+                }
+            }
+
+        }.resume()
     }
     
     
